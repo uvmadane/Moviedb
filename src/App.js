6acecb4dd874686cf8f397dbc33757bd1df5e751
umesh madane <umeshmadane2001@gmail.com>
2024-06-18 11:12:39 +0530

    Added 404 page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import UpcomingPage from "./pages/UpcomingPage"
 import MovieDetailPage from "./pages/MovieDetailPage"
 import SearchPage from "./pages/SearchPage"
 import Header from "./Components/Header"
+import NotFoundPage from "./pages/NotFounfPage"
 
 function App() {
   return (
     <>
-    <Header/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/toprated" element={<TopRatedPage />} />
         <Route path="/upcoming" element={<UpcomingPage />} />
         <Route path="/movieDetails/:id" element={<MovieDetailPage />} />
         <Route path="/search" element={<SearchPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   )
